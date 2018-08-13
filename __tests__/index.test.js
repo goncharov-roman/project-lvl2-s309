590719136229959c26538a5d4443d95cdf5a38cc
goncharov-roman <romg92@mail.ru>
2018-08-13 18:45:34 +0300

    tests lines of code reduced

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,55 +3,12 @@ import genDiff from '../src';
 
 const dir = `${__dirname}/__fixtures__`;
 
-const templateTest = (extension, outputFormat) => {
-  const file1 = `${dir}/before.${extension}`;
-  const file2 = `${dir}/after.${extension}`;
-  const expected = fs.readFileSync(`${dir}/expected-${outputFormat}.txt`, 'utf-8').trim();
-  const result = genDiff(file1, file2, outputFormat);
-  return { result, expected };
-};
-
-test('tree format json test', () => {
-  const { result, expected } = templateTest('json', 'tree');
-  expect(result).toBe(expected);
-});
-
-test('plain format json test', () => {
-  const { result, expected } = templateTest('json', 'plain');
-  expect(result).toBe(expected);
-});
-
-test('json format json test', () => {
-  const { result, expected } = templateTest('json', 'json');
-  expect(result).toBe(expected);
-});
-
-test('tree format yaml test', () => {
-  const { result, expected } = templateTest('yml', 'tree');
-  expect(result).toBe(expected);
-});
-
-test('plain format yaml test', () => {
-  const { result, expected } = templateTest('yml', 'plain');
-  expect(result).toBe(expected);
-});
-
-test('json format yaml test', () => {
-  const { result, expected } = templateTest('yml', 'json');
-  expect(result).toBe(expected);
-});
-
-test('tree format ini test', () => {
-  const { result, expected } = templateTest('ini', 'tree');
-  expect(result).toBe(expected);
-});
-
-test('plain format ini test', () => {
-  const { result, expected } = templateTest('ini', 'plain');
-  expect(result).toBe(expected);
-});
-
-test('json format ini test', () => {
-  const { result, expected } = templateTest('ini', 'json');
-  expect(result).toBe(expected);
+['json', 'yml', 'ini'].forEach((format) => {
+  ['tree', 'plain', 'json'].forEach(outputFormat => test(`${outputFormat} format ${format} test`, () => {
+    const file1 = `${dir}/before.${format}`;
+    const file2 = `${dir}/after.${format}`;
+    const expected = fs.readFileSync(`${dir}/expected-${outputFormat}.txt`, 'utf-8').trim();
+    const result = genDiff(file1, file2, outputFormat);
+    expect(result).toBe(expected);
+  }));
 });
